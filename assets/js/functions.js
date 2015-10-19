e82c4e00244c310a33f76fa89918a4ba831a37ac
Savagebanana <adriandornellas@gmail.com>
2015-10-19 04:32:29 -0400

    added javascript mobile nav menu functionality, and smooth scroll

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,62 @@
+// -----------------------------------------------------------------------------
+// Global functionality
+// -----------------------------------------------------------------------------
+
+(function ($) {
+
+    "use strict"
+
+    var $body,
+        $window,
+        settings = {
+            scrollDuration: 300
+        };
+
+    function onMobileToggleClick (e) {
+        var $this = $(this),
+            $navigation = $('.mobile-nav');
+
+        $this.toggleClass('is-open');
+        $navigation.toggleClass('is-open');
+    }
+
+    function onBlankLinkClick (e) {
+        var $this = $(this),
+            href = $this.attr('href'),
+            $target = $(href);
+
+        if ($target.length == 0)
+            return;
+
+        e.preventDefault();
+
+        $body.animate({
+            scrollTop: $target.offset().top
+        }, settings.duration);
+    }
+
+    function bindings () {
+        // Toggle mobile navigation (mobileNav)
+        $('.mobile-nav-toggle').on('click', onMobileToggleClick);
+
+        // Smooth scroll (smoothScroll)
+        $('a[href^="#"]').on('click', onBlankLinkClick);
+    }
+
+    $(document).ready(function () {
+        // Variable definitions
+        $body = $('html, body');
+        $window = $(window);
+
+        bindings();
+    });
+
+})(jQuery);
+
+// -----------------------------------------------------------------------------
+// Header Image Parallax
+// -----------------------------------------------------------------------------
+
 
 function headerImageScroll() {
 	var wScroll = $(window).scrollTop();
@@ -10,4 +69,3 @@ function headerImageScroll() {
 $(window).scroll(function(){
 	headerImageScroll();
 });
-
