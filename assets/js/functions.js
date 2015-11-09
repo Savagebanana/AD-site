7257a1da63e8222da5f47a5e7e8e75d528ccbe61
Savagebanana <adriandornellas@gmail.com>
2015-11-09 12:08:04 -0500

    mobile nav toggle fix

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,57 +1,20 @@
+
+
 // -----------------------------------------------------------------------------
-// Global functionality
+// mobile Nav Toggle
 // -----------------------------------------------------------------------------
+function mobileNav() {
+  $('.mobile-nav-toggle').on('click', function(){
+    var status = $(this).hasClass('is-open');
+    if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
+    else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
+  });
+}
 
-(function ($) {
+$(function() {
+  mobileNav();
+});
 
-    "use strict"
-
-    var $body,
-        $window,
-        settings = {
-            scrollDuration: 300
-        };
-
-    function onMobileToggleClick (e) {
-        var $this = $(this),
-            $navigation = $('.mobile-nav');
-
-        $this.toggleClass('is-open');
-        $navigation.toggleClass('is-open');
-    }
-
-    function onBlankLinkClick (e) {
-        var $this = $(this),
-            href = $this.attr('href'),
-            $target = $(href);
-
-        if ($target.length == 0)
-            return;
-
-        e.preventDefault();
-
-        $body.animate({
-            scrollTop: $target.offset().top
-        }, settings.duration);
-    }
-
-    function bindings () {
-        // Toggle mobile navigation (mobileNav)
-        $('.mobile-nav-toggle').on('click', onMobileToggleClick);
-
-        // Smooth scroll (smoothScroll)
-        $('a[href^="#"]').on('click', onBlankLinkClick);
-    }
-
-    $(document).ready(function () {
-        // Variable definitions
-        $body = $('html, body');
-        $window = $(window);
-
-        bindings();
-    });
-
-})(jQuery);
 
 // -----------------------------------------------------------------------------
 // Header Image Parallax
